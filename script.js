2aad922e2306b421f4bbb579bcd58de69f28103e
gavin-love <1co.loco@oprotonmail.ch>
2018-03-31 12:59:06 -0600

    Add event listeners
    Added jQuery event listeners for all inputs, buttons, and values

---
+++ script.js
@@ -0,0 +1,42 @@
+ideaBox();
+
+
+funtion ideaBox() {
+  var $titleInput = $('.title-input');
+  var $bodyInput = $('.body-input');
+  var $saveButton = $('.sumbit-button'); 
+  var $searchInput = $('.search-input');
+  var $cardTitle = $('.card-title');
+  var $cardBody = $('.card-body');
+  var $deleteButton = $('.delete-button');
+  var $qualityValue = $('.quality-value'); 
+  var $upVoteButton = $('.upvote-button');
+  var $downVoteButton = $('.downvote-button');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+};
+
